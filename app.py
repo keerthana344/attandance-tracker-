8e47d92c3c18865c8f569ec710984a9250856ea7
keerthana344 <keerthiyogesh771@gmail.com>
2026-02-04 10:28:23 +0530

    my first git push

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Explicitly defining host and port as requested
+    app.run(debug=True, host='127.0.0.1', port=5000)
